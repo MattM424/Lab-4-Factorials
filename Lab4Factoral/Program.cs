2ff7cbfdd6b2079548b0b74626ba143b2c620d5a
MattM424 <ginger424901@gmail.com>
2018-01-24 16:32:43 -0500

    First commit

---
+++ Lab4Factoral/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab4Factoral
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number");
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine(GetFactorial(n));
+
+
+        }
+
+        public static long GetFactorial(int n)
+        {
+            long acc = n;
+            for (int i = 1; i <= n; i++)
+            {
+                acc *= i;
+            }
+            return acc;
+        }
+    }
+}
